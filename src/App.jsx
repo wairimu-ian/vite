c34fe0707fa6163a3aed10ef864b3a93ac1ef398
wairimu-ian <wairimuian20@gmail.com>
2025-12-09 21:24:47 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function ian(){
+  let cars= ['BMW','mobius','toyota','lotus'];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  let phones= ['iphone','samsung','tecno','huawei','xiaomi']
+
+  let motorbikes= ['honda','dugati','dayun','BMW','shinery','kawasaki']
+  //const name="ian"
+  //return (<p>my name is {name}</p>);
+
+  //return(<ul>{value.splice(0,4)}</ul>)
+return(
+  <div>
+  <ul><h3>cars</h3>
+    {cars.map(car =>(<li>{car}</li>))}
+  </ul>
+
+  <ul><h3> phones</h3> 
+     {phones.map(phone =>(<li>{phone}</li>))}
+ </ul>
+
+  <ul><h3>bikes</h3>
+    {motorbikes.map(bike =>(<li>{bike}</li>))}
+
+  </ul>
+ </div>
+);
+
 }
 
-export default App
+export default ian
